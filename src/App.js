ff9f8678cd7b6eef642e19937f50186b83eadfce
Iheanacho Victor <viheanaco@gmail.com>
2020-09-12 12:14:03 +0100

    fetching data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        { id: 1, name: "Frankeinstein" },
-        { id: 2, name: "Dracula" },
-        { id: 3, name: "Zombie" },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
   }
 
   render() {
